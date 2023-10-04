506e4b83b424e5b8c2cb6210668b00a8ed4410a7
hyeinee3 <86913926+hyeinee3@users.noreply.github.com>
2023-10-04 10:23:42 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,26 +6,26 @@ resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/22"]
   location            = "koreacentral"
-  resource_group_name = "DevOps-ResourceGroup"
+  resource_group_name = "qahi"
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal_subnet"
-  resource_group_name  = "DevOps-ResourceGroup"
+  resource_group_name  = "qahi"
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_public_ip" "pip" {
   name                = "${var.prefix}-pip"
-  resource_group_name = "DevOps-ResourceGroup"
+  resource_group_name = "qahi"
   location            = "koreacentral"
   allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
-  resource_group_name = "DevOps-ResourceGroup"
+  resource_group_name = "qahi"
   location            = "koreacentral"
 
   ip_configuration {
@@ -38,7 +38,7 @@ resource "azurerm_network_interface" "main" {
 
 resource "azurerm_network_interface" "internal" {
   name                      = "${var.prefix}-nic2"
-  resource_group_name       = "DevOps-ResourceGroup"
+  resource_group_name       = "qahi"
   location                  = "koreacentral"
 
   ip_configuration {
@@ -51,7 +51,7 @@ resource "azurerm_network_interface" "internal" {
 
 resource "azurerm_linux_virtual_machine" "main" {
   name                            = "${var.prefix}-vm"
-  resource_group_name             = "DevOps-ResourceGroup"
+  resource_group_name             = "qahi"
   location                        = "koreacentral"
   size                            = "Standard_D1_v2"
   admin_username                  = "${var.username}"
